9b460c34ac26d94f238c51e44601a5beb6e3e34a
Kevin Lam <kevinlamx11@gmail.com>
2025-09-23 18:51:06 -0400

    Task 1: Add Ignite button

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,84 @@
 import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
-// This widget is the root of your application.
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-// Application name
-title: 'Rocket Launch Controller',
-theme: ThemeData(
-primarySwatch: Colors.blue,
-),
-// A widget that will be started on the application startup
-home: CounterWidget(),
-);
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Application name
+      title: 'Rocket Launch Controller',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // A widget that will be started on the application startup
+      home: CounterWidget(),
+    );
+  }
 }
-}
+
 class CounterWidget extends StatefulWidget {
-@override
-_CounterWidgetState createState() => _CounterWidgetState();
+  const CounterWidget({super.key});
+
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
 }
+
 class _CounterWidgetState extends State<CounterWidget> {
-//set counter value
-int _counter = 0;
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: const Text('Rocket Launch Controller'),
-),
-//set up the widget alignement
-body: Column(
-mainAxisAlignment: MainAxisAlignment.center,
-children: [
-Center(
-child: Container(
-color: Colors.blue,
-child: Text(
-//to displays current number
-'$_counter',
-style: TextStyle(fontSize: 50.0),
-),
-),
-),
-Slider(
-min: 0,
-max: 100,
-value: _counter.toDouble(),
-onChanged: (double value) {
-setState(() {
-_counter = value.toInt();
-});
-},
-activeColor: Colors.blue,
-inactiveColor: Colors.red,
-),
-],
-),
-);
-}
+  // set counter value
+  int _counter = 0;
+
+  // Task 1: method to increase counter by 1
+  void _ignite() {
+    setState(() {
+      // clamp value so it never goes above 100
+      _counter = (_counter + 1).clamp(0, 100);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rocket Launch Controller'),
+      ),
+      // set up the widget alignment
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              color: Colors.blue,
+              child: Text(
+                // to displays current number
+                '$_counter',
+                style: TextStyle(fontSize: 50.0),
+              ),
+            ),
+          ),
+
+          // Ignite button (Task 1)
+          ElevatedButton(
+            onPressed: _ignite,
+            child: const Text("Ignite +1"),
+          ),
+
+          // slider to adjust the counter value
+          Slider(
+            min: 0,
+            max: 100,
+            value: _counter.toDouble(),
+            onChanged: (double value) {
+              setState(() {
+                _counter = value.toInt();
+              });
+            },
+            activeColor: Colors.blue,
+            inactiveColor: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
 }
